260b8477131901c9819b9e8ebe7c135aeb3231f6
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2023-12-06 13:26:57 +0530

    Update MainActivity.java

--- Music Player/MainActivity.java
+++ Music Player/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.musicplayerapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
